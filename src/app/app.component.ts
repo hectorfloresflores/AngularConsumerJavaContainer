75ced43be4032f47e8273fb8c0612f141a27b271
hectorfloresflores <48966777+hectorfloresflores@users.noreply.github.com>
2020-09-19 12:42:52 -0500

    Project creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import {interval} from 'rxjs/internal/observable/interval';
 
+// @ts-ignore
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+@Injectable()
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) { }
+  console:Object;
   title = 'JavaApiFrontEnd';
+  configUrl = 'https://heroku-java-api.herokuapp.com/';
+  ngOnInit(): void {
+   // this.getConfig().subscribe(data => {
+   //   console.log(data);
+   // });
+
+    interval(3000).subscribe(() => {
+      this.getConfig().subscribe(data =>{
+        console.log(data);
+        this.console =  data.UsedHeapMemory;
+      });
+    });
+  }
+
+
+  // tslint:disable-next-line:typedef
+  getConfig() {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '/'
+      })
+    };
+    const value = this.http.get(this.configUrl, options);
+
+    return value;
+  }
+
+
+
 }
